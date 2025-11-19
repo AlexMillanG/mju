c1ced234b9674ba101551fc2af4b07631a687203
AlexMillanG <20223tn072@utez.edu.mx>
2025-11-19 14:26:17 -0600

    add: some services

--- src/main/java/mx/edu/utez/gmudeployment/modules/user/UserRepository.java
+++ src/main/java/mx/edu/utez/gmudeployment/modules/user/UserRepository.java
@@ -3,6 +3,9 @@ package mx.edu.utez.gmudeployment.modules.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepository extends JpaRepository<Long, UserEntity> {
+public interface UserRepository extends JpaRepository<UserEntity,Long> {
+
 }
--- src/main/java/mx/edu/utez/gmudeployment/modules/user/UserService.java
+++ src/main/java/mx/edu/utez/gmudeployment/modules/user/UserService.java
@@ -4,8 +4,9 @@ import lombok.RequiredArgsConstructor;
 import mx.edu.utez.gmudeployment.utils.ApiResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +16,19 @@ public class UserService {
 
     public ResponseEntity<ApiResponseDto> findAll(){
         return new ResponseEntity<>(new ApiResponseDto(userRepository.findAll(),false,null), HttpStatus.OK);
+    }
+
+    public ResponseEntity<ApiResponseDto> findById(Long id){
+
+        Optional<UserEntity> foundUser = userRepository.findById(id);
+        if (foundUser.isEmpty()){
+            return new ResponseEntity<>(new ApiResponseDto(null,true,"usuario no encontrado"),HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(new ApiResponseDto(foundUser.get(),false,null),HttpStatus.OK);
+    }
+
+    public ResponseEntity<ApiResponseDto> save (){
+        //aldrick es gay
+        return null;
     }
 }
